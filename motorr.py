75ef25e1a6bf891dd316b8ee3324eb57a063d72c
colldbeer <131449771+colldbeer@users.noreply.github.com>
2023-05-06 23:43:45 +1000

    Update motorr.py

--- motorr.py
+++ motorr.py
@@ -14,13 +14,16 @@ def control_motor():
     while True:
         try:
             if aveTemp < temp - 2:
-
+                board.pwm(5,180)
+                time.sleep(3)
                 print("Temperature recorded is less than the goal, direction of fan is set to move heat into room")
             if temp -2 <= aveTemp <= temp + 2:
-                board.digital_write(5,0)
-                board.digital_write(6,0)
+                board.pwm_write(5,0)
+                board.pwm_write(6,0)
+                time.sleep(3)
             if aveTemp >= temp + 2:
-                
+                board.pwm(6,180)
+                time.sleep(3)
                 print("Temperature recorded is more than the goal, direction of fan is set to move heat out of the room")
         except KeyboardInterrupt:
             board.shutdown
