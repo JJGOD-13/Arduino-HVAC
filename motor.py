b4f40141fe11de54733ed0ab2b301f9b70b04e71
mmmayyy <131100058+mmmayyy@users.noreply.github.com>
2023-05-07 18:24:05 +1000

    Update motor.py

--- motor.py
+++ motor.py
@@ -1,17 +1,6 @@
 from pymata4 import pymata4
 
-
-fanPin1 = 5 #pwm
-fanPin2 = 6 #pwm
-
-board_motor = pymata4.Pymata4()
-
-board_motor.set_pin_mode_pwm_output(fanPin1)
-board_motor.set_pin_mode_pwm_output(fanPin2)
-board_motor.set_pin_mode_pwm_output(fanPin1)
-board_motor.set_pin_mode_pwm_output(fanPin2)
-
-def control_motor(board,direction = 'clockwise',speed = 0): 
+def control_motor(direction = 'clockwise',speed = 0): 
     """
     This function turns on the motor to a given speed and direction
 
@@ -19,6 +8,10 @@ def control_motor(board,direction = 'clockwise',speed = 0):
     :Param speed: integer between 100 and 255
     :return: None
     """
+    from polling_loop import board,fanPin1,fanPin2
+    board.set_pin_mode_pwm_output(fanPin1)
+    board.set_pin_mode_pwm_output(fanPin2)
+
     if direction == 'clockwise':
         board.pwm_write(fanPin1,speed)
         board.pwm_write(fanPin2,0)
